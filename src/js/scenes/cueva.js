a0bc0a1fdeaf51976688eb3df791a0ee5aa9ee6e
pali509 <pablcamp@ucm.es>
2023-04-24 20:41:32 +0200

    Dialogos hechos + portales

--- src/js/scenes/cueva.js
+++ src/js/scenes/cueva.js
@@ -1,7 +1,13 @@
+
 /**
  * Escena de Título.
  * @extends Phaser.Scene
  */
+
+//import * as Matter from 'akelarre/node_modules/matter-js/build/matter.js';
+let hablando = false;
+let length = 0;
+
 export default class Cueva extends Phaser.Scene {
 	/**
 	 * Escena principal.
@@ -24,16 +30,19 @@ export default class Cueva extends Phaser.Scene {
 
         //Cargar NPCs
         this.load.spritesheet('bruja', 'src/assets/Personajes/bruja3.png', { frameWidth: 32, frameHeight: 32 });
-
+        this.load.spritesheet('motos', 'src/assets/Personajes/PabloM.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('gato', 'src/assets/Personajes/gatete.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('vacio','src/assets/vacio.png', { frameWidth: 32, frameHeight: 32 });
         //Cargar prota
         this.load.spritesheet('amaia', 
             'src/assets/Personajes/Prota.PNG',
             { frameWidth: 34, frameHeight: 34 }
         );
+        //Cargar portales:
+        this.load.image('portal1', 'src/assets/Portal1.png');
+        this.load.image('portal2', 'src/assets/Portal2.png');
+        this.load.image('portal3', 'src/assets/Portal3.png');
 
-        this.load.plugin('DialogModalPlugin', './dialog.js');
-        //this.sys.install('DialogModalPlugin');
-        //console.log(this.sys.dialogModal);
     }
 	
 	/**
@@ -51,9 +60,9 @@ export default class Cueva extends Phaser.Scene {
         layer.setCollisionByExclusion([-1 , 0]);
 
         //Prota
-        this.player = this.physics.add.sprite(1550, 450, 'amaia').setScale(2);
-        this.player.setSize(15, 15);
-        this.player.body.offset.y = 16;
+        this.player = this.physics.add.sprite(400, 450, 'amaia').setScale(2);
+        this.player.setSize(15, 13);
+        this.player.body.offset.y = 20;
         this.physics.add.collider(this.player, layer);
         this.player.setDepth(2);
         
@@ -61,36 +70,150 @@ export default class Cueva extends Phaser.Scene {
         this.bruja = this.physics.add.sprite(1555, 390, 'bruja').setScale(2);
         this.bruja.setSize(15, 15);
         this.bruja.setDepth(1);
- 
         this.bruja.body.offset.y = 16;
-
         this.bruja.body.immovable = true;
 
+        this.motos = this.physics.add.sprite(1595, 1100, 'motos').setScale(2);
+        this.motos.setSize(13, 20);
+        this.motos.setDepth(1);
+        this.motos.body.offset.x = 5;
+        this.motos.body.offset.y = 7;
+        this.motos.body.immovable = true;
+
+        this.gato = this.physics.add.sprite(690, 1360, 'gato').setScale(2);
+        this.gato.setSize(13, 20);
+        this.gato.setDepth(1);
+        this.gato.body.offset.x = 5;
+        //this.gato.body.offset.y = 7;
+        this.gato.body.immovable = true;
+
+        //Objetos interactuables
+        this.estanteria = this.physics.add.sprite(400, 390, 'vacio').setScale(2);
+        this.estanteria.setSize(25,27);
+        this.estanteria.body.offset.x = 8;
         
-        /*Para colisiones voy a necesitar:
-        -Arriba para Pablo Motos y bruja
-        -Izquierda para Gato
-        */
-        //Colisiones con bruja
+        this.caldero = this.physics.add.sprite(465, 400, 'vacio').setScale(2);
+        this.caldero.setSize(15,20);
+        this.caldero.body.offset.x = 8;
+
+        this.portal1 = this.physics.add.sprite(1030, 540, 'portal1').setScale(0.6);
+        this.portal1.setSize(50,50);
+        this.portal1.body.offset.x = 90;
+        this.portal1.body.offset.y = 150;
+        this.portal1.setVisible(false);
+
+        this.portal2 = this.physics.add.sprite(805, 730, 'portal2').setScale(0.6);
+        this.portal2.setSize(50,50);
+        this.portal2.body.offset.x = 90;
+        this.portal2.body.offset.y = 150;
+        this.portal2.setVisible(false);
+
+        this.portal3 = this.physics.add.sprite(1252, 730, 'portal3').setScale(0.6);
+        this.portal3.setSize(50,50);
+        this.portal3.body.offset.x = 90;
+        this.portal3.body.offset.y = 150;
+        this.portal3.setVisible(false);
+
+        //Pantallita del texto
+        let graphics = this.add.graphics({x: 1200, y: 600});
+        graphics.fillStyle(0x000000, 0.8);
+        graphics.fillRect(0, 0, 700, 100);
+        graphics.lineStyle(4, 0x000000, 1);
+        graphics.strokeRect(0, 0, 700, 100);
+        
+        //El texto
+        let text = this.add.text(400, 400, "Este es el texto de la ventana", { font: "24px Arial", fill: "#ffffff" });
+        
+        //Gráfico adicional
+        let graphicsNombre = this.add.graphics({x: graphics.x, y: graphics.y - 30});
+        graphicsNombre.fillStyle(0x000000, 0.8);
+        graphicsNombre.fillRect(0, 0, 150, 30);
+        graphicsNombre.lineStyle(4, 0x000000, 1);
+        graphicsNombre.strokeRect(0, 0, 150, 30);
+        
+        //Texto adicional
+        let textNombre = this.add.text(400, 400, 'AAAAA', {fontFamily: 'Arial', fontSize: '16px', color: '#ffffff'});
+        textNombre.setOrigin(0.5);
+        
+        //Configuracion basica de la pantalla y texto
+        graphics.setVisible(false);
+        graphicsNombre.setVisible(false);
+        text.setVisible(false);
+        textNombre.setVisible(false);
+        hablando = false;
+
+        //Teclas para dialogo
+        var espacio = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        //var escape = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
+        //Dialogos:
+
+        const dialogosBruja = [
+            "¡Ah! Hola...",
+            "Mis hermanas me han mandado quedarme aqui vigilandote, no  \nme esperaba que me encontraras...",
+            "... Lo siento mucho por ti, es una pena que tu vida vaya a \nacabar tan pronto por nuestra culpa.",
+            "A mis hermanas les encanta realizar sacrificios humanos pero \nla verdad es que a mi no me gusta nada...",
+            "Ojala pudiera dejarte ir, pero mis hermanas de dan demasiado \nmiedo...",
+            "¡Espera! He tenido una idea, yo no puedo dejarte salir, \n¡Pero si que puedo abrir los portales que llevan a las llaves!",
+            "Por desgracia tu huida seguirá siendo algo muy dificil de \nconseguir...",
+            "Tras los portales se encuentran tres pruebas muy dificiles \nque tendrás que superar si quieres conseguir las llaves.",
+            "Estoy segura de que podrás lograrlo, y si no pues bueno... \nIbas a morir de todas formas...",
+            "Voy a abrir los portales, ¡Consigue las llaves deprisa! Si \nmis hermanas terminan los preparativos antes de que huyas...",
+            "No quiero ni imaginarme como harían para que tu sacrificio \nfuera incluso más doloroso, pero seguro que ellas sabrían como.",
+            "No te preocupes por lo que me pueda pasar a mi cuando se \nenteren de que abrí los portales.",
+            "¡Ya iba siendo hora de que les plantase cara a mis hermanas!",
+            "Los portales ya están abiertos, ¡Mucha suerte! Y que Belzebut \neste contigo...",
+        ];
+        const dialogosMotos = [
+            "Ey nena, ¿Qué hace una chica como tu en un sitio como este?",
+            "Ja ja ja ja, *coff* *coff*...",
+            "¿Yo? Pues nada, estaba dandome un paseo de turisteo cuando \nderrepente vi a las mozas estas recogiendo plantas por la zona.",
+            "Con lo buenas que están me paré para hablarlas a ver si caia \nalguna 'minita', ya sabes ja ja ja.",
+            "Cuando me dijeron que querían llevarme a la cueva yo tan \ninocente pensé que habia triunfado, ¡Y mirame ahora!",
+            "Me dijeron que iban a ofrecerme a su Dios, pero creo que al \nfinal pude convencerlas de que estaba demasiado bueno \npara eso.",
+            "Desde ese momento me han dejado aquí en una esquina, no se \na que esperan para dejarme ir ja ja ja ja...",
+            "¿No tendrás unas galletas dinosaurus por ahí o algo no? \nMe muero de hambre.",
+            "Aunque ahora que te tengo delante me apetecen otras cosas \ntambién ja ja ja *coff coff*",
+            "Deduzco que a ti tambien te han capturado para un \nsacrificio ¿No?",
+            "Que te parece nena, ¿Hacemos que nuestros ultimos momentos \nvivos sean divertidos? ;)"
+        ];
+
+        const dialogosGato =[
+            "¡Hola, Bienvenida a la cueva!",
+            "Soy el fiel amigo y servidor de las brujas, \n¡Y me lo paso pipa viendo los sacrificios humanos que hacen!",
+            "Tu eres la siguiente en la lista. Veo que eres \nmuy joven, pobrecita que poco has vivido...",
+            "JA JA JA JA JA, ¡Que pringada!",
+            "disfruta de tus ultimos momentos paseando por \nla cueva si quieres.",
+            "O quedate quieta hasta que vuelvan mis amas, \n¡Me da completamente igual!"
+        ];
+
+        const dialogosEstanteria =[
+            "Hay un montón de libros de hechiceria y herbologia...",
+            "'El necronomicon', 'Sacrificios101', \n'Plantas venenosas y como usarlas'...",
+            "Que mal rollo..."
+        ];
+        const dialogosCaldero =[
+            "Supongo que con esto hacen sus pociones, que esteriotípico",
+            "Esta burbujeando, me pregunto que efecto \ntendría si lo bebo",
+            "Huele fatal..."
+        ];
+
+        this.addOverlap(this.player,this.bruja, graphics, text, dialogosBruja, graphicsNombre, textNombre,"Bruja", espacio);
+        this.addOverlap(this.player,this.motos, graphics, text, dialogosMotos, graphicsNombre, textNombre,"Pablo Motos", espacio);
+        this.addOverlap(this.player,this.gato, graphics, text, dialogosGato, graphicsNombre, textNombre,"Gato", espacio);
+        this.addOverlap(this.player,this.estanteria, graphics, text, dialogosEstanteria, graphicsNombre, textNombre,"Amaia", espacio);
+        this.addOverlap(this.player,this.caldero, graphics, text, dialogosCaldero, graphicsNombre, textNombre,"Amaia", espacio);
+
+        /*
+
+                
+        //Colisiones con bruja -- Sin usar temporalmente
         this.collisionUp = false;
         this.collisionDown = false;
         this.collisionLeft = false;
         this.collisionRight = false;
 
-        
 
-        this.physics.add.collider(this.player,this.bruja, () =>{
-            console.log('colision');
-            var espacio = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-            espacio.on('down', function(event) {
-                // Crear ventana emergente con el diálogo
-                console.log('dialogo?');
-               
-            }, this);
-        
-        });
-
-        /*
         this.physics.add.collider(this.player, this.bruja, () =>{
             const playerBounds = this.player.getBounds();
             const brujaBounds = this.bruja.getBounds();
@@ -188,43 +311,83 @@ export default class Cueva extends Phaser.Scene {
 
     }
 
-    showDialog() {
-        // Mostrar el texto de diálogo si el jugador está en colisión con el personaje
-          this.dialogText.setText('Tonto el que lo lea');
-          console.log('Deberia de estar saliendo el texto');
-          this.dialogText.setVisible(true);
+    addOverlap(player, npc, graphics, text, dialogo, graphicsN, textNombre, nombre, espacio) {
+        /*COSAS POR HACER:
+            -Cambiar nombre al personaje cuando se presente (+ Añadir esa frase a los dialogos)
+            -Animar el texto
+            -Que se vea en el mismo lado de la pantalla todo para los 3
+        */
+        this.physics.add.overlap(player, npc, () => {
+
+          graphics.setPosition(npc.x - 355,npc.y + 160);
+          graphicsN.setPosition(graphics.x, graphics.y - 30);
+          text.setPosition(npc.x - 350,npc.y + 180);
+          textNombre.setPosition(graphicsN.x + 50, graphicsN.y + 15);
+
+          textNombre.setText(nombre);
+          
+          if(Phaser.Input.Keyboard.JustDown(espacio)) {
+            graphics.setVisible(true);
+            graphicsN.setVisible(true);
+            text.setVisible(true);
+            textNombre.setVisible(true);
+            hablando = true;
+
+            if (length < dialogo.length) {
+              text.setText(dialogo[length]);
+              console.log(dialogo[length]);
+              console.log(length);
+              length++;
+            }
+            else{
+            if(textNombre.text == "Bruja"){
+                this.portal1.setVisible(true);
+                this.portal2.setVisible(true);
+                this.portal3.setVisible(true);
+            }
+            graphics.setVisible(false);
+            graphicsN.setVisible(false);
+            text.setVisible(false);
+            textNombre.setVisible(false);
+            hablando = false;
+            length = 0;
+            }
+        }      
+
+        });
       }
 
-
-
-	/**
-	* Loop del juego
-	*/
+	
+	//Loop del juego
     update(){
 
-        if (this.cursors.right.isDown && !this.collisionRight){
-            this.player.setVelocityX(100);
+
+         //console.log(this.player.x);
+         //console.log(this.player.y);
+
+        if (this.cursors.right.isDown && !hablando){
+            this.player.setVelocityX(128);
             this.player.setVelocityY(0);
             this.player.anims.play('right_amaia', true);
             this.animation = 1;
 
         }
-        else if (this.cursors.left.isDown && !this.collisionLeft){
-            this.player.setVelocityX(-100);
+        else if (this.cursors.left.isDown && !hablando){
+            this.player.setVelocityX(-128);
             this.player.setVelocityY(0);
             this.player.anims.play('left_amaia', true);
             this.animation = 2;
 
         }
-        else if (this.cursors.up.isDown && !this.collisionUp){
-            this.player.setVelocityY(-100);
+        else if (this.cursors.up.isDown && !hablando){
+            this.player.setVelocityY(-128);
             this.player.setVelocityX(0);
             this.player.anims.play('up_amaia', true);
             this.animation = 3;
 
         }
-        else if (this.cursors.down.isDown && !this.collisionDown){
-            this.player.setVelocityY(100);
+        else if (this.cursors.down.isDown && !hablando){
+            this.player.setVelocityY(128);
             this.player.setVelocityX(0);
             this.player.anims.play('down_amaia', true);
             this.animation = 4;
--- src/js/scenes/dialog.js
+++ src/js/scenes/dialog.js
@@ -1,96 +1,243 @@
-var DialogModalPlugin = function (scene) {
-    // the scene that owns the plugin
-    this.scene = scene;
-    this.systems = scene.sys;
-    if (!scene.sys.settings.isBooted) {
-      scene.sys.events.once('boot', this.boot, this);
-    }
-  };
-  // Register this plugin with the PluginManager
-  DialogModalPlugin.register = function (PluginManager) {
-    PluginManager.register('DialogModalPlugin', DialogModalPlugin, 'dialogModal');
-  };
-  DialogModalPlugin.prototype = {
-    // called when the plugin is loaded by the PluginManager
-    boot: function () {
-      var eventEmitter = this.systems.events;
-      eventEmitter.on('shutdown', this.shutdown, this);
-      eventEmitter.on('destroy', this.destroy, this);
-    },
-    //  Called when a Scene shuts down, it may then come back again later
-    // (which will invoke the 'start' event) but should be considered dormant.
-    shutdown: function () {},
-    // called when a Scene is destroyed by the Scene Manager
-    destroy: function () {
-      this.shutdown();
-      this.scene = undefined;
-    },
-    // Initialize the dialog modal
-    init: function (opts) {
-        // Check to see if any optional parameters were passed
-        if (!opts) opts = {};
-        // set properties from opts object or use defaults
-        this.borderThickness = opts.borderThickness || 3;
-        this.borderColor = opts.borderColor || 0x907748;
-        this.borderAlpha = opts.borderAlpha || 1;
-        this.windowAlpha = opts.windowAlpha || 0.8;
-        this.windowColor = opts.windowColor || 0x303030;
-        this.windowHeight = opts.windowHeight || 150;
-        this.padding = opts.padding || 32;
-        this.closeBtnColor = opts.closeBtnColor || 'darkgoldenrod';
-        this.dialogSpeed = opts.dialogSpeed || 3;
-        // used for animating the text
-        this.eventCounter = 0;
-        // if the dialog window is shown
-        this.visible = true;
-        // the current text in the window
-        this.text;
-        // the text that will be displayed in the window
-        this.dialog;
-        this.graphics;
-        this.closeBtn;
-        // Create the dialog window
-        this._createWindow();
-    },
-    // Gets the width of the game (based on the scene)
-    _getGameWidth: function () {
-        return this.scene.sys.game.config.width;
-    },
-    // Gets the height of the game (based on the scene)
-    _getGameHeight: function () {
-        return this.scene.sys.game.config.height;
-    },
-    // Calculates where to place the dialog window based on the game size
-    _calculateWindowDimensions: function (width, height) {
-        var x = this.padding;
-        var y = height - this.windowHeight - this.padding;
-        var rectWidth = width - (this.padding * 2);
-        var rectHeight = this.windowHeight;
-        return {
-        x,
-        y,
-        rectWidth,
-        rectHeight
-        };
-    },
-    // Creates the inner dialog window (where the text is displayed)
-    _createInnerWindow: function (x, y, rectWidth, rectHeight) {
-        this.graphics.fillStyle(this.windowColor, this.windowAlpha);
-        this.graphics.fillRect(x + 1, y + 1, rectWidth - 1, rectHeight - 1);
-    },
-    // Creates the border rectangle of the dialog window
-    _createOuterWindow: function (x, y, rectWidth, rectHeight) {
-        this.graphics.lineStyle(this.borderThickness, this.borderColor, this.borderAlpha);
-        this.graphics.strokeRect(x, y, rectWidth, rectHeight);
-    },
-    // Creates the dialog window
-    _createWindow: function () {
-        var gameHeight = this._getGameHeight();
-        var gameWidth = this._getGameWidth();
-        var dimensions = this._calculateWindowDimensions(gameWidth, gameHeight);
-        this.graphics = this.scene.add.graphics();
-        this._createOuterWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
-        this._createInnerWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
-    },
-  };
-  
+export class DialogBox {
+  constructor(scene) {
+      this.scene = scene;
+  }
+
+  /**
+   * 
+   * @param {Object.<>} opts - Ajustes opcionales de las cajas de texto, por si se quiere cambiar el formato
+   * @param {number} opts.borderThickness Grueso de los bordes de la caja de texto
+   * @param {hexadecimal_number} opts.borderColor Color de los bordes de la caja de texto
+   * @param {number} opts.borderAlpha Canal alpha de los bordes de la caja de texto (transparencia)
+   * @param {number} opts.windowAlpha Canal alpha de la caja de texto (transparencia)
+   * @param {hexadecimal_number} opts.windowColor Color de la caja de texto
+   * @param {number} opts.windowHeight Altura de la caja de texto
+   * @param {number} opts.padding Padding del texto
+   * @param {number} opts.dialogSpeed Velocidad de aparición de los textos
+   * @param {number} opts.x Posición en X de la caja de texto
+   * @param {number} opts.y Posición en Y de la caja de texto
+   * @param {boolean} opts.visible Color del texto en la caja de texto
+   */
+  init(opts) {
+      if (!opts) opts = {}; 
+      this.borderThickness = opts.borderThickness || 3;
+      this.borderColor = opts.borderColor || 0x000000;
+      this.borderAlpha = opts.borderAlpha || 1; 
+      this.windowAlpha = opts.windowAlpha || 0.8; 
+      this.windowColor = opts.windowColor || 0x303030;
+      this.windowHeight = opts.windowHeight || 150; 
+      this.padding = opts.padding || 32; 
+      this.dialogSpeed = opts.dialogSpeed || 10;
+      this.x = opts.x || this.padding;
+      this.y = opts.y;
+      this.eventCounter = 0; //Contador de eventos
+      this.visible = true; //Variable que dice si el cuadro de texto es visible o no
+      this.text; //Referencia a Phaser.Text
+      this.textColor = opts.textColor;
+      this.dialog; //Referencia a Phaser.Dialog
+      this.graphics; //Referencia a Phaser.Graphics
+      this.beingAnimated; //Nos dice si el texto está siendo o no activado
+      this.createWindow(); //Crear la ventana
+  }
+
+  /**
+   * Devuelve el ancho de la ventana del canvas
+   * @returns El ancho de la ventana del canvas
+   */
+  getGameWidth() {
+      return this.scene.sys.game.config.width;
+  }
+
+  /**
+   * Devuelve el largo de la ventana del canvas
+   * @returns El largo de la ventana del canvas
+   */
+  getGameHeight() {
+      return this.scene.sys.game.config.height;
+  }
+
+  /**
+   * Calcula las dimensiones de la caja de texto
+   * @param {number} width El ancho de la ventana del canvas
+   * @param {number} height El alto de la ventana del canvas
+   * @returns {Object.<number, number, number, number>} Objeto con el rectángulo con las dimensiones de la caja de texto
+   */
+  calculateWindowDimensions(width, height) {
+      var x = this.x;
+      var y = this.y || height - this.windowHeight - this.padding;
+      var rectWidth = width - (this.padding * 2);
+      var rectHeight = this.windowHeight;
+      return {
+          x,
+          y,
+          rectWidth,
+          rectHeight
+      };
+  }
+
+  /**
+   * Crea la ventana interior, es decir, el recuadro en el que se mostrará el texto
+   * @param {number} x X en la que se va a crear el recuadro
+   * @param {number} y Y en la que se va a crear el recuadro
+   * @param {number} rectWidth Anchura del recuadro
+   * @param {number} rectHeight Altura del recuadro
+   */
+  createInnerWindow(x, y, rectWidth, rectHeight) {
+      this.graphics.fillStyle(this.windowColor, this.windowAlpha);
+      this.graphics.fillRect(x + 1, y + 1, rectWidth - 1, rectHeight - 1);
+  }
+
+  /**
+   * Crea la ventana exterior, es decir, los bordes de la ventana
+   * @param {number} x X en la que se van a crear los bordes
+   * @param {number} y Y en la que se van a crear los bordes
+   * @param {number} rectWidth Altura del recuadro
+   * @param {number} rectHeight Anchura del recuadro
+   */
+  createOuterWindow(x, y, rectWidth, rectHeight) {
+      this.graphics.lineStyle(this.borderThickness, this.borderColor, this.borderAlpha);
+      this.graphics.strokeRect(x, y, rectWidth, rectHeight);
+  }
+
+  /**
+   * Crea el cuadro de texto
+   */
+  createWindow() {
+      var gameHeight = this.getGameHeight();
+      var gameWidth = this.getGameWidth();
+      var dimensions = this.calculateWindowDimensions(gameWidth, gameHeight);
+      this.graphics = this.scene.add.graphics();
+      this.createOuterWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
+      this.createInnerWindow(dimensions.x, dimensions.y, dimensions.rectWidth, dimensions.rectHeight);
+  }
+
+  /**
+   * Muestra u oculta el cuadro de texto
+   */
+  toggleWindow() {
+      this.visible = !this.visible;
+      if (this.text) this.text.visible = this.visible;
+      if (this.graphics) this.graphics.visible = this.visible;
+      if (this.closeBtn) this.closeBtn.visible = this.visible;
+      if (this.timedEvent) this.timedEvent.remove();
+      if (this.text) this.text.destroy();
+  }
+
+  /**
+   * Setea el texto a mostrar en pantalla
+   * @param {string} text Texto a mostrar
+   * @param {boolean} animate Selecciona si queremos o no animar el texto
+   */
+  setText(text, animate) {
+      this.eventCounter = 0;
+      this.dialog = text.split('');
+      if (this.timedEvent) this.timedEvent.remove();
+
+      var tempText = animate ? '' : text;
+
+      this.textPosition(tempText);
+      this.text.setColor(this.textColor);
+      this.text.setFontFamily('Silkscreen');
+      if (animate) {
+          this.timedEvent = this.scene.time.addEvent({
+          delay: 150 - (this.dialogSpeed * 30),
+          callback: this.animateText,
+          callbackScope: this,
+          loop: true
+          });
+      }
+  }
+
+  /**
+   * Anima el texto
+   */
+  animateText() {
+      this.beingAnimated = true;
+      this.eventCounter++;
+      this.text.setText(this.text.text + this.dialog[this.eventCounter - 1]);
+      if (this.eventCounter === this.dialog.length) {
+        this.timedEvent.remove();
+        this.scene.time.addEvent({
+          delay: 180,
+          callback: this.endTextAnimation,
+          callbackScope: this,
+          loop: false
+      });
+      }
+  }
+
+  endTextAnimation() {
+      this.beingAnimated = false;
+  }
+
+  /**
+   * Calcular la posición en la que debe ir el texto
+   * @param {string} text Texto a mostrar
+   */
+  textPosition(text) {
+      if (this.text) this.text.destroy();
+      var x = this.x + 10|| this.padding + 10;
+      var y = this.y + 10|| this.getGameHeight() - this.windowHeight - this.padding + 10;
+      this.text = this.scene.make.text({
+          x,
+          y,
+          text,
+          color : this.textColor,
+          style: {
+            wordWrap: { width: this.getGameWidth() - (this.padding * 2) - 25 }
+          }
+      });
+  }
+}
+
+export default class DialogScene extends Phaser.Scene {
+  constructor() {
+      super({key: 'dialog', active: true, visible: true});
+      this.dialogBox = new DialogBox(this);
+      this.nameBox = new DialogBox(this);
+      this.hasCreatedWindow = false;
+      this.isToggled = true;
+      this.event = false;
+  }
+
+  update() {
+      if(this.dialogBox.beingAnimated && !this.event) {
+          this.events.emit("isBeingAnimated");
+          this.event = true;
+      }
+      else if (!this.dialogBox.beingAnimated && this.event) {
+          this.events.emit("isNotBeingAnimated");
+          this.event = false;
+      }
+  }
+
+  createWindow() {
+      this.dialogBox.init({
+          textColor : "#FFFFFF",
+          windowColor: 0x292929
+      });
+      this.nameBox.init({
+          borderThickness : 3,
+          windowColor: 0xE3BE39,
+          windowHeight: 35,
+          padding: 290,
+          x:20,
+          y:380,
+          textColor: "#FFFFFF"
+      });
+      
+      this.hasCreatedWindow = true;
+  }
+
+  setText(character, text, animate) {
+      this.dialogBox.setText(text, animate);
+      this.nameBox.setText(character, false);
+      
+  }
+
+  toggleWindow() {
+      this.dialogBox.toggleWindow();
+      this.nameBox.toggleWindow();
+      this.isToggled = !this.isToggled;
+  }
+}
