214e0896e1bddbe06187720220eedb1042dc1b4d
pali509 <pablcamp@ucm.es>
2023-04-27 20:17:12 +0200

    Intro casi + Entrada a portales
    Falta que se muevan al final de la intro pero xd

--- src/js/game.js
+++ src/js/game.js
@@ -3,7 +3,12 @@
  */
 import GoatRun from './scenes/goatrun.js';
 import Cueva from './scenes/cueva.js';
-import pociones from './scenes/avoidthepotions.js'
+import AvoidThePotions from './scenes/avoidthepotions.js';
+import Intro from './scenes/intro.js';
+
+var gameManager = {
+    keys : 0
+};
 
 let config = {
     type: Phaser.CANVAS,
@@ -11,17 +16,19 @@ let config = {
     //parent: "juego",
     width:  800,
     height: 600,
+    keys : 0, //Piezas de llaves para los minijuegos
     pixelArt: true,
 	scale: {
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
 		mode: Phaser.Scale.FIT,
+        
 		min: {
             width: 328,
             height: 188
         },
 		max: {
-            width: 2312,
-            height: 752
+            width: 1112,
+            height: 552
         },
 		zoom: 1
     },
@@ -29,7 +36,7 @@ let config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: true
+            debug: false
         },
         checkCollision: {
             up: true,
@@ -38,9 +45,9 @@ let config = {
             right: true
         }
     },
-    scene: [pociones],
+    scene: [Intro,Cueva, GoatRun, AvoidThePotions],
     title: "Akelarre",
     version: "0.0.1"
 };
 
-new Phaser.Game(config);
+new Phaser.Game(config, gameManager);
