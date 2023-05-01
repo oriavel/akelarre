378f4649d5649e40f050ae4749394a3ab76e71e9
Carlos Martín <79253407+eskei-ucm@users.noreply.github.com>
2023-05-01 20:25:58 +0200

    Done

--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,6 @@
 /**
  * Clase Game de Phaser: crear e iniciar juego
  */
-<<<<<<< Updated upstream
-import GoatRun from './scenes/goatrun.js';
-import Cueva from './scenes/cueva.js';
-import pociones from './scenes/avoidthepotions.js'
-=======
 import GoatRun from './scenes/GoatRun/goatrun.js';
 import Cueva from './scenes/cueva.js';
 import AvoidThePotions from './scenes/AvoidThePotions/avoidthepotions.js';
@@ -14,7 +9,6 @@ import Intro from './scenes/intro.js';
 var gameManager = {
     keys : 0
 };
->>>>>>> Stashed changes
 
 let config = {
     type: Phaser.CANVAS,
@@ -22,30 +16,19 @@ let config = {
     //parent: "juego",
     width:  800,
     height: 600,
-<<<<<<< Updated upstream
-=======
     keys : 0, //Piezas de llaves para los minijuegos
->>>>>>> Stashed changes
     pixelArt: true,
 	scale: {
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
 		mode: Phaser.Scale.FIT,
-<<<<<<< Updated upstream
-=======
         
->>>>>>> Stashed changes
 		min: {
             width: 328,
             height: 188
         },
 		max: {
-<<<<<<< Updated upstream
-            width: 2312,
-            height: 752
-=======
             width: 1112,
             height: 552
->>>>>>> Stashed changes
         },
 		zoom: 1
     },
@@ -53,11 +36,7 @@ let config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-<<<<<<< Updated upstream
             debug: true
-=======
-            debug: false
->>>>>>> Stashed changes
         },
         checkCollision: {
             up: true,
@@ -66,19 +45,11 @@ let config = {
             right: true
         }
     },
-<<<<<<< Updated upstream
-    scene: [pociones],
-=======
 scene: [Intro, Cueva, GoatRun, AvoidThePotions],
 
 
->>>>>>> Stashed changes
     title: "Akelarre",
     version: "0.0.1"
 };
 
-<<<<<<< Updated upstream
-new Phaser.Game(config);
-=======
 new Phaser.Game(config, gameManager);
->>>>>>> Stashed changes
