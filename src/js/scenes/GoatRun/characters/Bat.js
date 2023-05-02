96926150b044693921c8482803ff31960921c022
danicobos01 <dcobos01@ucm.es>
2023-05-02 21:12:23 +0200

    .

--- src/js/scenes/GoatRun/characters/Bat.js
+++ src/js/scenes/GoatRun/characters/Bat.js
@@ -29,7 +29,7 @@ export default class Bat extends Phaser.GameObjects.Sprite {
 
     // Detecta a que distancia está del personaje para bajar e ir a por ella
     movimiento_bats(){
-        if(Math.abs(this.scene.player.body.position.x - this.body.position.x) < 400){
+        if(Math.abs(this.scene.player.body.position.x - this.body.position.x) < 430){
             if(this.body.position.y > 215){
                 this.body.velocity.y += 0.4; 
             }
